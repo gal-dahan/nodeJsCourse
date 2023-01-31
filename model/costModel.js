0aafaf9b7eb98c5dc3b42f0afc5a3b982c03e6b9
gal-dahan <963gal963@gmail.com>
2023-01-31 14:47:39 +0200

    Update costModel.js

--- model/costModel.js
+++ model/costModel.js
@@ -8,7 +8,6 @@ const costSchema = new mongoose.Schema({
     year: { type: Number, min: 1970, max: 2100 },
     month: { type: Number, min: 0, max: 11 },
     day: { type: Number, min: 0, max: 31 },
-    id: { type: Number, required: true, unique: true },
     description: { type: String, maxlength: 100 },
     category: { type: String, enum: ['food', 'health', 'housing', 'sport', 'education', 'transportation', 'other'] },
     sum: { type: Number, min: 0 }
